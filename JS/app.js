2fcbb0a2e32ef2e929c0e8436b70f21b62a71249
Ulifly <ulifly@hotmail.com>
2023-12-12 23:55:29 -0600

    adapting no modules

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,28 @@
-import pokemonCatch from './catch.js';
+async function obtenerInformacionPokemon(nombre) {
+  try {
+    const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`);
+    if (respuesta.ok) {
+      const datosPokemon = await respuesta.json();
+      // Aquí puedes procesar los datos del Pokémon como desees
+      return datosPokemon;
+    } else {
+      throw new Error('No se encontró información para el Pokémon');
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
 
-// const container = document.querySelector('#pokecard');
-// container.innerHTML = `
-//    `;
+const nombrePokemon = 'pikachu';
+obtenerInformacionPokemon(nombrePokemon)
+  .then((informacionPokemon) => {
+    if (informacionPokemon) {
+      console.log(`Información de ${nombrePokemon}:`);
+      console.log(`Nombre: ${informacionPokemon.name}`);
+      console.log(`Altura: ${informacionPokemon.height}`);
+      console.log(`Peso: ${informacionPokemon.weight}`);
+    } else {
+      console.log(`No se encontró información para el Pokémon ${nombrePokemon}`);
+    }
+  });

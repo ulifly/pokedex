fd5ae8c61e195657798821cb1118a54434ab15f8
Ulifly <ulifly@hotmail.com>
2023-12-16 10:00:05 -0600

    modulos aplicados

--- JS/app.js
+++ JS/app.js
@@ -1,32 +1,13 @@
 const botonback = document.querySelector('#bback');
 const botonnext = document.querySelector('#bnext');
 const botonrand = document.querySelector('#brand');
+import getpoke from "./getpoke.js";
 
 botonrand.addEventListener('click', pokerand); 
 
 function pokerand() {
   let pokeid = Math.floor(Math.random() * 898) + 1;
   getpoke(pokeid);
-}
-
-
-function getpoke(pokeid) {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${pokeid}`)
-  .then(response => response.json())
-  .then(poke => {
-    const nombre = poke.name;
-    const img = poke.sprites.front_default;
-    const num = poke.id;
-    console.log(nombre, img);
-    document.getElementById('pokecard').innerHTML = `
-      <div class ="card">  
-        <h2 id="pokeid">${num}</h2>
-        <img src="${img}" alt="${nombre}">
-        <h1>${nombre}</h1>
-      </div>
-    `
-  })
-  .catch(error => console.log('Error', error));
 }
 
 
--- JS/getpoke.js
+++ JS/getpoke.js
@@ -1,12 +1,18 @@
-export default function pokemonCatch(args) {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${args}`)
-    .then((response) => response.json())
-    .then((data) => {
-      const pokemon = {
-        name: data.name,
-        image: data.sprites.front_default,
-      };
-      return pokemon;
-    })
-    .catch((error) => console.error(error));
+export default function getpoke(pokeid) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokeid}`)
+  .then(response => response.json())
+  .then(poke => {
+    const nombre = poke.name;
+    const img = poke.sprites.front_default;
+    const num = poke.id;
+    console.log(nombre, img);
+    document.getElementById('pokecard').innerHTML = `
+      <div class ="card">  
+        <h2 id="pokeid">${num}</h2>
+        <img src="${img}" alt="${nombre}">
+        <h1>${nombre}</h1>
+      </div>
+    `
+  })
+  .catch(error => console.log('Error', error));
 }

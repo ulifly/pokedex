3122475b02900b921e1a4dc1c8d705f94a4fc358
Ulifly <ulifly@hotmail.com>
2023-12-18 08:20:06 -0600

    se grega fucnion de colores por tipo

--- JS/pokeinfo.js
+++ JS/pokeinfo.js
@@ -1,4 +1,29 @@
 export default function pokeinfo(pokeid) {
+  let colorType = '#B09398'
+
+  let colorTypes = {
+    grass: '#4A9681',
+    fire: '#FF675C',
+    water: '#0596C7',
+    bug: '#A2FAA3',
+    normal: '#B09398',
+    poison: '#795663',
+    electric: '#FFEA70',
+    ground: '#D2B074',
+    fairy: '#FFC6D9',
+    flying: '#7AE7C7',
+    ghost: '#561D25',
+    dragon: '#DA627D',
+    steel: '#1D8A99',
+    fighting: '#2F2F2F',
+    dark: '#2A1A1F',
+    unknown: '#2A1A1F',
+    shadow: '#2A1A1F',
+    ice: '#AFEAFD',
+    rock: '#999799',
+    psychic: '#FFC6D9'
+  };
+  
   fetch(`https://pokeapi.co/api/v2/pokemon/${pokeid}`)
   .then(response => response.json())
   .then(poke => {
@@ -8,6 +33,10 @@ export default function pokeinfo(pokeid) {
     const hab = poke.abilities;
     const types = poke.types;
     const stats = poke.stats;
+
+    let typeColors = types.map(type => colorTypes[type.type.name]);
+    let colorType = typeColors[0];
+    
     document.getElementById('pokecard').innerHTML = `
       <div class="wrap-bg" style="overflow: hidden; position: relative;">
         <img class="poke-bg" src="${img}" style="
@@ -28,7 +57,7 @@ export default function pokeinfo(pokeid) {
         </ul>
         <h3>Tipos:</h3>
         <ul>
-          ${types.map(t => `<li>${t.type.name}</li>`).join('')}
+        ${types.map((t, index) => `<li class="tipos" style="color:${typeColors[index]};" >${t.type.name}</li>`).join('')}
         </ul>
         <h3>Estadísticas:</h3>
         <ul>
@@ -40,3 +69,4 @@ export default function pokeinfo(pokeid) {
   })
   .catch(error => console.log('Error', error));
 }
+
